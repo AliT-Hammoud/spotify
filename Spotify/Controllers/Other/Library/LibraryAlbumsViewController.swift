1d8bfab128d879bda6e1d0a54c13fde2d22063aa
AliT-Hammoud <ali.ha@totersapp.com>
2021-12-10 13:32:58 +0200

    part-21/Library UI vertical scroll view with button and indicators

---
+++ Spotify/Controllers/Other/Library/LibraryAlbumsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LibraryAlbumsViewController.swift
+//  Spotify
+//
+//  Created by Ali Hammoud on 12/10/21.
+//
+
+import UIKit
+
+class LibraryAlbumsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemOrange
+
+    }
+
+}
---
+++ Spotify/Controllers/Other/Library/LibraryPlaylistsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LibraryPlaylistsViewController.swift
+//  Spotify
+//
+//  Created by Ali Hammoud on 12/10/21.
+//
+
+import UIKit
+
+class LibraryPlaylistsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+    
+
+}
